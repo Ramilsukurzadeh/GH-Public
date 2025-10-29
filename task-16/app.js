a556c0ae673b102d23e908ac590f7b61829f8d68
Ramilsukurzadeh <ramil.sukurzadeh@gmail.com>
2025-10-29 14:37:46 +0400

    task16

---
+++ task-16/app.js
@@ -0,0 +1,23 @@
+// 1)
+
+console .log("Hello world");
+
+// 2)
+
+const my_name = "Ramil Shukurzade"
+console.log(my_name);
+
+// 3)
+
+// person / $add - istifadə oluna bilən dəyişənlərdi çünki person - hərflə başlayıb add isə $ işarəsi ilə.
+
+// 2user / c@rt / titile color / console - dəyişənləri rəqəm, @ işarəsi və dəyərlər istifadə oluna bilməz.
+
+
+// 4)
+
+const name = prompt("Enter your name")
+console .log(name)
+
+const surName = prompt("Enter your surname")
+console .log(surName)
