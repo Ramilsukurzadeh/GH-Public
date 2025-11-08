8a9dbc4c1f8ae3905984a596cf6096da6f328ccb
Ramilsukurzadeh <ramil.sukurzadeh@gmail.com>
2025-11-08 13:25:06 +0400

    task20

---
+++ task-20/app.js
@@ -0,0 +1,29 @@
+// 1)
+
+// const firstItem = document.querySelector("Ul li");
+// console.log(firstItem.textContent);
+
+// 2)
+
+// document.querySelector("h1").remove();
+// const ul = document.querySelector("ul");
+// const newItem = document.createElement("li");
+// newItem.textContent = "TITAN RTX";
+// ul.appendChild(newItem);
+
+// 3)
+
+// document.querySelector("h1").textContent = "GeForce 20 Series";
+// const primaryItems = document.querySelectorAll(".primary");
+
+// for (let i = 0; i < primaryItems.length; i++) {
+//     primaryItems[i].style.backgroundColor = "#f1f1f1";
+// }
+
+
+// 4)
+// const Items = document.querySelectorAll(".primary");
+
+// for (let i = 0; i < Items.length; i++) {
+//     console.log(Items[i].textContent);
+// }
